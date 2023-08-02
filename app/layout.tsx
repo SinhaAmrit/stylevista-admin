40fa54be90af8ea17eaff5abe8da25e12c91031f
Amrit Sinha <purpleplots@gmail.com>
2023-08-02 20:05:13 +0530

    feat - auth done, style - modal added

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ModalProvider } from "@/providers/modal.provider";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Style Vista | Dashboard",
+  description: "by Amrit Sinha",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { Modal } from "@/components/ui/modal";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+  name: z.string().min(3),
+});
+
+export const StoreModal = () => {
+  const storeModal = useStoreModal();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    // TODO add api call here for creating the store
+  };
+
+  return (
+    <Modal
+      title="Create Store"
+      description="Add a new store to manage products and categories"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    >
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Store Name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                <Button variant="outline" onClick={storeModal.onClose}>
+                  Cancel
+                </Button>
+                <Button type="submit">Continue</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </Modal>
+  );
+};
